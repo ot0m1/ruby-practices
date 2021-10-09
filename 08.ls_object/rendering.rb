942951bdb3ff6957a1bd2fbdafbfc460d81da0c6
ot0m1 <coding_addiction@yahoo.co.jp>
2021-10-10 08:36:00 +0900

    画面出力処理に不具合がったのでRenderingを修正

--- 08.ls_object/rendering.rb
+++ 08.ls_object/rendering.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Rendering
-  COLUMN_NUMBER = 3
+  MAX_COLUMN_LENGTH = 3
 
   def initialize(params, files)
     @params = params
@@ -9,18 +9,20 @@ class Rendering
   end
 
   def output
-    @params['l'] ? render_long : render_short
+    result = @params['l'] ? render_long : render_short
+    puts result
   end
 
   private
 
   def render_short
-    max_file_name_count = @files.map { |file| File.basename(file).size }.max
+    max_file_name_count = @files.map(&:size).max
     ajusted_files = adjust_to_max_file_name_count(@files, max_file_name_count)
-    max_column_count = COLUMN_NUMBER
 
-    ajusted_files.each_slice(max_column_count).map do |file|
-      puts file.join(' ')
+    if ajusted_files.length <= MAX_COLUMN_LENGTH
+      ajusted_files.join(' ')
+    else
+      tabulate(ajusted_files, max_file_name_count)
     end
   end
 
@@ -30,12 +32,35 @@ class Rendering
     end
   end
 
+  def tabulate(files, max_file_name_count)
+    row_count = (files.count.to_f / MAX_COLUMN_LENGTH).ceil
+    transposed_files = safe_transpose(files.each_slice(row_count).to_a)
+    format_table(transposed_files, max_file_name_count)
+  end
+
+  def safe_transpose(files)
+    files[0].zip(*files[1..-1])
+  end
+
+  def format_table(files, max_file_name_count)
+    files.map do |file|
+      render_short_format_row(file, max_file_name_count)
+    end.join("\n")
+  end
+
+  def render_short_format_row(files, max_file_path_count)
+    files.map do |file|
+      basename = file || ''
+      basename.ljust(max_file_path_count + 1)
+    end.join.rstrip
+  end
+
   def render_long
     analysed_data = FileAnalyser.new(@files).analyse
     block_total = analysed_data.sum { |data| data[:blocks] }
     head = "total #{block_total}"
     body = make_long_format_body(analysed_data)
-    puts [head, *body].join("\n")
+    [head, *body].join("\n")
   end
 
   def make_long_format_body(analysed_data)
