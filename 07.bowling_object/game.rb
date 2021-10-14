46ae1f09922bcc174a33a6fc69a887c15cce85b2
ot0m1 <coding_addiction@yahoo.co.jp>
2021-10-14 19:57:47 +0900

    Gameクラスの不必要な変数をリファクタリング

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -6,22 +6,18 @@ class Game
   end
 
   def score
-    point = 0
-
-    (0..9).each do |count|
+    (0..9).sum do |count|
       current_frame, next_frame, after_next_frame = @frames.slice(count, 3)
       next_frame ||= Frame.new(Shot.new(0))
       after_next_frame ||= Frame.new(Shot.new(0))
 
-      point += if current_frame.strike?
-                 current_frame.sum + Frame.sum_next_first_two_shots(next_frame, after_next_frame)
-               elsif current_frame.spare?
-                 current_frame.sum + next_frame.first_shot.numerate
-               else
-                 current_frame.sum
-               end
+      if current_frame.strike?
+        current_frame.sum + Frame.sum_next_first_two_shots(next_frame, after_next_frame)
+      elsif current_frame.spare?
+        current_frame.sum + next_frame.first_shot.numerate
+      else
+        current_frame.sum
+      end
     end
-
-    point
   end
 end
