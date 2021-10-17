b2e8d1f15f3b6eb4291450b985c134bd7facc818
ot0m1 <coding_addiction@yahoo.co.jp>
2021-10-17 19:46:30 +0900

    tabulateメソッドをFileCollecterクラスからRanderingクラスに持ってきた

--- 08.ls_object/file_collecter.rb
+++ 08.ls_object/file_collecter.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class FileCollecter
-  MAX_COLUMN_LENGTH = 3
-
   def initialize(params)
     row_files = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
     @files = params['r'] ? row_files.sort.reverse : row_files.sort
@@ -14,13 +12,7 @@ class FileCollecter
       file.ljust(max_file_name_size_count)
     end
   end
-
-  def tabulate(ajusted_files)
-    row_count = (ajusted_files.count.to_f / MAX_COLUMN_LENGTH).ceil
-    transposed_files = safe_transpose(ajusted_files.each_slice(row_count).to_a)
-    format_table(transposed_files, max_file_name_size_count)
-  end
-
+  
   def make_long_format_body
     max_sizes = %i[nlink user group size mtime].map do |key|
       @analysed_files.find_max_size(key)
@@ -34,26 +26,7 @@ class FileCollecter
     "total #{@analysed_files.block_total}"
   end
 
-  private
-
   def max_file_name_size_count
     @files.map(&:size).max
-  end
-
-  def safe_transpose(files)
-    files[0].zip(*files[1..-1])
-  end
-
-  def format_table(files, max_file_name_count)
-    files.map do |file|
-      render_short_format_row(file, max_file_name_count)
-    end.join("\n")
-  end
-
-  def render_short_format_row(files, max_file_path_count)
-    files.map do |file|
-      basename = file || ''
-      basename.ljust(max_file_path_count + 1)
-    end.join.rstrip
   end
 end
