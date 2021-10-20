272bf1950bd0e0b633932e496f3bf74f3ea75d12
ot0m1 <coding_addiction@yahoo.co.jp>
2021-10-20 19:53:14 +0900

    FileCollecterクラスのmax_file_name_size_countメソッドまわりをリファクタリング

--- 08.ls_object/file_collecter.rb
+++ 08.ls_object/file_collecter.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 class FileCollecter
+  attr_reader :max_file_name_size
+
   def initialize(params)
     row_files = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
     @files = params['r'] ? row_files.sort.reverse : row_files.sort
+    @max_file_name_size = max_file_name_size_count
     @analysed_files = FileAnalyser.new(@files) if params['l']
   end
 
   def ajustted_files
     @files.map do |file|
-      file.ljust(max_file_name_size_count)
+      file.ljust(@max_file_name_size)
     end
   end
 
@@ -25,6 +28,8 @@ class FileCollecter
   def make_long_format_head
     "total #{@analysed_files.block_total}"
   end
+
+  private
 
   def max_file_name_size_count
     @files.map(&:size).max
--- 08.ls_object/rendering.rb
+++ 08.ls_object/rendering.rb
@@ -18,10 +18,10 @@ class Rendering
     ajusted_files = @file_collection.ajustted_files
     row_count = (ajusted_files.count.to_f / MAX_COLUMN_LENGTH).ceil
     transposed_files = safe_transpose(ajusted_files.each_slice(row_count).to_a)
-    max_file_name_size_count = @file_collection.max_file_name_size_count
+    max_file_name_size = @file_collection.max_file_name_size
 
     transposed_files.map do |file|
-      render_short_format_row(file, max_file_name_size_count)
+      render_short_format_row(file, max_file_name_size)
     end.join("\n")
   end
 
